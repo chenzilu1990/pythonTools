4f03e0f271c77e4150a5e485a40c808760682880
Chen ZiLu <2822459649@qq.com>
2021-09-30 13:47:28 +0800

    ffwe

---
+++ http/test.py
@@ -0,0 +1,3 @@
+import httpTool
+
+httpTool.startHttp()
--- imgBlend/test.py
+++ imgBlend/test.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python3
 import os
 import imgLib as imgTool
-
 
 for file in os.listdir('./imgs'):
 	imgTool.toWebp('./imgs/' + file)
 
 for file in os.listdir('./imgs'):
 	imgTool.blendWithFlag('./imgs/' + file)
+
